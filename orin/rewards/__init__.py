d2248aadcc4d5a40fcf95d53ba01161a2ef5793b
Selim Özten <selimozten@hotmail.com>
2026-03-08 21:36:18 +0300

    export SharpeReward and AdaptiveCompositeReward from rewards module

--- orin/rewards/__init__.py
+++ orin/rewards/__init__.py
@@ -1,8 +1,16 @@
 """Reward functions for financial text environments."""
 
 from orin.rewards.calibration import CalibrationReward
-from orin.rewards.composite import CompositeReward
+from orin.rewards.composite import AdaptiveCompositeReward, CompositeReward
 from orin.rewards.directional import DirectionalReward
 from orin.rewards.protocol import RewardFn
+from orin.rewards.sharpe import SharpeReward
 
-__all__ = ["DirectionalReward", "CalibrationReward", "CompositeReward", "RewardFn"]
+__all__ = [
+    "AdaptiveCompositeReward",
+    "CalibrationReward",
+    "CompositeReward",
+    "DirectionalReward",
+    "RewardFn",
+    "SharpeReward",
+]
