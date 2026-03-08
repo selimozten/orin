6fe0aaa36d991cfe12d555fafe10b010b4e1564f
Selim Özten <selimozten@hotmail.com>
2026-03-08 21:36:37 +0300

    export split_data from data module

--- orin/data/__init__.py
+++ orin/data/__init__.py
@@ -1,10 +1,17 @@
 """Data loading and source connectors for orin environments."""
 
-from orin.data.loaders import load_dataframe, load_huggingface, load_jsonl, load_sample_data
+from orin.data.loaders import (
+    load_dataframe,
+    load_huggingface,
+    load_jsonl,
+    load_sample_data,
+    split_data,
+)
 
 __all__ = [
     "load_jsonl",
     "load_sample_data",
     "load_dataframe",
     "load_huggingface",
+    "split_data",
 ]
